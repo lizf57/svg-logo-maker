b6831eb4a6829f9037f8ca2b89d2be8ffdc96b56
mona lisa <lizfriedman5@gmail.com>
2023-08-14 10:01:49 -0500

    pseudo code/directory structure/license craeted

---
+++ index.js
@@ -0,0 +1,18 @@
+// import inquirer
+const inquirer = require('inquirer');
+
+// import file system
+const fs = require('fs');
+
+// import SVG logo examples
+const fileName = 
+
+// import questions
+const questions = require()
+
+// import new shape from user input
+const setShape = require()
+
+
+
+
